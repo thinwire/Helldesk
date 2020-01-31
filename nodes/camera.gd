b6b62cf781975932c9eaeb74a5c79065a1a3dd2d
Patrik Lindström <patrik@js-games.com>
2020-01-31 23:57:42 +0200

    Make basic camera work

--- nodes/camera.gd
+++ nodes/camera.gd
@@ -1,10 +1,13 @@
-extends Node
+extends Camera
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 onready var player = get_parent().find_node("player");
+
+var height = 8;
+var distance = 6;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +18,7 @@ func _ready():
 func _process(delta):
 	
 	var p = player.translation;
-	self.translation.x = p.x;
-	self.translation.y = p.y + 4;
-	self.translation.z = p.z + 10;
-	
-	pass
+	var from = Vector3(p.x, p.y + height, p.z + distance) - player.velocity * 0.1;
+	#self.look_at_from_position(p, p, Vector3.UP);
+	self.translation = from;
+	self.look_at(p, Vector3.UP);
